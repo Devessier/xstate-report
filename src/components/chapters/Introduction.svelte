faff8437b8b96a08a11f52fe43ffa221f370a8cf
Devessier <baptiste@devessier.fr>
2023-07-24 13:58:52 +0200

    feat: design first page

--- src/components/chapters/Introduction.svelte
+++ src/components/chapters/Introduction.svelte
@@ -10,8 +10,26 @@
 
 <Chapter>
   <Page>
-    <div class="h-full w-full flex justify-center items-center">
-      <h1>State of XState 2023</h1>
+    <div class="absolute inset-0">
+      <img
+        src="/CleanShot 2023-07-17 at 20.04.59@2x.png"
+        class="w-full h-full object-cover mt-0"
+        alt=""
+      />
+
+      <div
+        class="absolute inset-0 bg-white/[0.85] flex flex-col justify-center items-center"
+      >
+        <h1 class="text-6xl text-center mb-0">State of XState</h1>
+
+        <h2 class="text-center mt-6 text-4xl font-bold">2023</h2>
+
+        <h2
+          class="absolute inset-x-0 bottom-0 text-center text-3xl font-semibold mb-24"
+        >
+          Baptiste Devessier
+        </h2>
+      </div>
     </div>
   </Page>
 </Chapter>
