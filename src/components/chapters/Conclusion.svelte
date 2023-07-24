7b46172dd71e4bb617dea4bca59765708528cdb5
Devessier <baptiste@devessier.fr>
2023-07-24 19:28:19 +0200

    feat: write credits

--- src/components/chapters/Conclusion.svelte
+++ src/components/chapters/Conclusion.svelte
@@ -4,12 +4,21 @@
   import PageTitle from "../PageTitle.svelte";
 
   import { compiledContent as Part0 } from "./Conclusion/0.md";
+  import { compiledContent as Part1 } from "./Conclusion/1.md";
 </script>
 
 <Chapter chapter="Wrap up">
-  <Page>
+  <Page id="wrap-up">
     <PageTitle />
 
     {@html Part0()}
+  </Page>
+</Chapter>
+
+<Chapter chapter="Credits">
+  <Page id="credits">
+    <PageTitle />
+
+    {@html Part1()}
   </Page>
 </Chapter>
