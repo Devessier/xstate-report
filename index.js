f4020eb0564a2863610fae0d884b2e6c542e1abc
Devessier <baptiste@devessier.fr>
2023-09-30 19:33:46 +0200

    build: ensure script generates correct pdf

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const browser = await chromium.launch();
 const page = await browser.newPage();
 await page.goto("http://localhost:3000");
 
+await page.waitForLoadState('networkidle');
+
 await page.pdf({
     path: './XState Report.pdf',
-    margin: {
-        left: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-    },
     printBackground: true,
     preferCSSPageSize: true,
+    format: 'A4',
 });
 
 await browser.close();
