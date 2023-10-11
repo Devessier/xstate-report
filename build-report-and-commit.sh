ef1b82315cbf836d3cea4a91b82cd8f85343003f
Devessier <baptiste@devessier.fr>
2023-10-11 17:23:26 +0200

    ci: use a bash script for the release script

---
+++ build-report-and-commit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+npm run preview &
+
+until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
+    printf '.'
+    sleep 5
+done
+
+echo 'Server is listening';
+
+node ./build-report.js --production
+
+git add .
+git commit -m "chore: generate report"
+git push
